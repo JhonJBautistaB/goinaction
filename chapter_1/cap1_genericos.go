000928fc681d7adf3573864a48c49970d5f5ca51
JhonJBautistaB <jhon.bautista@outlook.com>
2025-09-14 18:35:41 -0500

    desarrollo de ejemplos dentro del primer capitulo del libro

---
+++ chapter_1/cap1_genericos.go
@@ -0,0 +1,21 @@
+// los genericos continuan donde finalizan las interfaces
+package main
+
+import "fmt"
+
+func SumNumbers[N int | float64](numberSlice ...N) N {
+	var total N
+	for i := range numberSlice {
+		total += numberSlice[i]
+	}
+	return total
+}
+
+func main() {
+	fmt.Println(SumNumbers(1, 2, 3))
+	fmt.Println(SumNumbers(1.1, 2.2, 3.3))
+}
+
+//output:
+// 6
+// 6.6
---
+++ chapter_1/cap1_interface.go
@@ -0,0 +1,57 @@
+// Las interfaces, a su vez, permiten que los tipos relacionados se usen indistintamente, siempre que tengan el mismo comportamiento (según lo definido por sus métodos). [2] Por ejemplo, puedes crear una interfaz, Describer, que represente cualquier cosa que tenga una descripción.
+package main
+
+import "fmt"
+
+type Describer interface {
+	Describe() string
+}
+
+func PrintDescription(d Describer) {
+	fmt.Println(d.Describe())
+}
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func (p Person) Name() string {
+	return p.FirstName + " " + p.LastName
+}
+
+func (p Person) Describe() string {
+	return fmt.Sprintf("%s is a %d year-old human", p.Name(), p.Age)
+}
+
+type Cat struct {
+	Name     string
+	Attitude string
+	Color    string
+}
+
+func (c Cat) Describe() string {
+	return fmt.Sprintf("%s is a %s cat (%s)", c.Name, c.Color, c.Attitude)
+}
+
+func main() {
+	andy := Person{
+		FirstName: "Andy",
+		LastName:  "Walker",
+		Age:       43,
+	}
+	barry := Cat{
+		Name:     "Barry",
+		Attitude: "the best lil' duder",
+		Color:    "black and white",
+	}
+	benny := Cat{
+		Name:     "Benny",
+		Attitude: "kind of a jerk",
+		Color:    "orange",
+	}
+	PrintDescription(andy)
+	PrintDescription(barry)
+	PrintDescription(benny)
+}
